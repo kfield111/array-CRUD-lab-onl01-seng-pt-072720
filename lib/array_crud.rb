f70ea11b75f5e15a0ebece82b48e4842f9fb8ef2
Kelin Field <kfield111@hotmail.com>
2020-08-01 22:56:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,12 +35,17 @@ end
 
 
 def retrieve_element_from_index(array, index_number)
-
+  test(2)
 end
+
+
 
 def retrieve_first_element_from_array(array)
 
 end
+
+
+
 
 def retrieve_last_element_from_array(array)
 
