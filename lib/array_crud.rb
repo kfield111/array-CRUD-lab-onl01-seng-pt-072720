dc571e5434838fedd1a518876106e5ce68f75eb8
Kelin Field <kfield111@hotmail.com>
2020-08-01 22:54:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,27 +2,37 @@ def create_an_empty_array
   []
 end
 
+
+
 def create_an_array
   test ["cat","dog","horse","frog"]
 end
 
+
+
 def add_element_to_end_of_array(array, element)
   test << "kitten"
-
 end
+
+
 
 def add_element_to_start_of_array(array, element)
   test.unshift("puppy")
-
 end
+
+
 
 def remove_element_from_end_of_array(array)
-
+  test.pop
 end
+
+
 
 def remove_element_from_start_of_array(array)
 
 end
+
+
 
 def retrieve_element_from_index(array, index_number)
 
